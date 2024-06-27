9d71f40128c16da13e416902d9994aacc92be9c9
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-06-27 10:35:13 +0300

    lab.md

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+// multiply//
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+//random//
+
+let random = Math.floor(Math.random() * 10) +1;
+
+
+//mod//
+
+let num3 = 400
+let num4 = 6
+let mod = num3 % num4
+
+//max//
+let max = Math.max(2, 10, 20)
